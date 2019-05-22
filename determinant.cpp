10911da98b322ed262246ca25f2ad652fc3b9c36
Raphael Costa <rapha2706@hotmai..com>
2019-05-22 15:03:03 -0300

    Forgot to delete parcial matrixes in confactor func

--- determinant.cpp
+++ determinant.cpp
@@ -2,7 +2,7 @@
 #include <cmath>
 using namespace std;
 
-#define N 4
+#define N 2
 
 double ** createTestMatrix(int n){
     double **m = new double *[n];
@@ -45,25 +45,17 @@ void printMatrix(double **m, int n){
 }
 
 double cofactor(double **matriz, int p, int n){
-    // printf("p: %d\n", p);
-    // matriz: matriz
-    // p: posicao que esta sendo calculada
-    // n: tamanho da matriz
     if(n == 1){
         return matriz[0][0];
     }
     else{
         int nextSize = n-1;
-        //primeiro: prepara matriz sem a linha e a coluna da posicao
-        //segundo: faz a determinante pro cofactor
-        // printf("Cofactor, p: %d, n: %d\n", p, n);
-        int g = 0; // helpers para inserir na nova matriz, g: linha e h: coluna
+        int g = 0;
         int h = 0;
         double **parcial = alocMatrix(n-1);
-        for(int i=1; i<n; i++){ //comeca do 1 pq sempre eliminamos a linha 0, pois fazemos laplace extensor pra primeira linha
+        for(int i=1; i<n; i++){
             for(int j=0; j<n; j++){
                 if(j != p){
-                    // printf("adding to parcial matrix. [%d, %d]: %f\n", g, h, matriz[i][j]);
                     parcial[g][h] = matriz[i][j];
                     h++;
                     if(h > nextSize-1){
@@ -73,12 +65,11 @@ double cofactor(double **matriz, int p, int n){
                 }
             }
         }
-        // printMatrix(parcial, n-1);
         double calc = 0;
         for (int i=0; i<nextSize; i++){
-            // printf("next cofactors: %f\n", parcial[0][i]);
             calc +=  pow(-1, p) * cofactor(parcial, i, nextSize);
         }
+        deleteMatrix(parcial, nextSize);
         return calc;
     }
 }
@@ -86,10 +77,9 @@ double cofactor(double **matriz, int p, int n){
 double determinant(double **matriz){
     double det = 0;
     double **m2;
-    // det = cofactor(matriz, 0, N);
-    for(int i = 0; i < N; i++){ // p/ cada elemento da primeira linha da matriz
+    for(int i = 0; i < N; i++){
         double cof = cofactor(matriz, i, N);
-        printf("Cof of %f: %f\n", matriz[0][i], cof);
+        printf("Cofactor of %f: %f\n", matriz[0][i], cof);
         det += matriz[0][i] * cof;
     }
     return det;
@@ -103,6 +93,5 @@ int main(){
     printf("Calculating Determinant for the matrix: \n");
     printMatrix(m1, N);
     std::cout << determinant(m1) << std::endl;
-
     deleteMatrix(m1, N);
 }
