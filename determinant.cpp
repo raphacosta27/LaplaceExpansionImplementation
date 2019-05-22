bf7a7ae98b35a25ce49218155951b995effd2560
Raphael Costa <rapha2706@hotmai..com>
2019-05-22 14:58:08 -0300

    Laplace extension working, starting mpi implementation

--- determinant.cpp
+++ determinant.cpp
@@ -2,7 +2,7 @@
 #include <cmath>
 using namespace std;
 
-#define N 4
+#define N 2
 
 double ** createTestMatrix(int n){
     double **m = new double *[n];
@@ -45,7 +45,7 @@ void printMatrix(double **m, int n){
 }
 
 double cofactor(double **matriz, int p, int n){
-    printf("p: %d\n", p);
+    // printf("p: %d\n", p);
     // matriz: matriz
     // p: posicao que esta sendo calculada
     // n: tamanho da matriz
@@ -56,13 +56,14 @@ double cofactor(double **matriz, int p, int n){
         int nextSize = n-1;
         //primeiro: prepara matriz sem a linha e a coluna da posicao
         //segundo: faz a determinante pro cofactor
-        printf("Cofactor, p: %d, n: %d\n", p, n);
-        int g, h = 0; // helpers para inserir na nova matriz, g: linha e h: coluna
-        double **parcial = alocMatrix(N);
-        for(int i=1; i<N; i++){ //comeca do 1 pq sempre eliminamos a linha 0, pois fazemos laplace extensor pra primeira linha
-            for(int j=0; j<N; j++){
+        // printf("Cofactor, p: %d, n: %d\n", p, n);
+        int g = 0; // helpers para inserir na nova matriz, g: linha e h: coluna
+        int h = 0;
+        double **parcial = alocMatrix(n-1);
+        for(int i=1; i<n; i++){ //comeca do 1 pq sempre eliminamos a linha 0, pois fazemos laplace extensor pra primeira linha
+            for(int j=0; j<n; j++){
                 if(j != p){
-                    // printf("adding to parcial matrix. [%d, %d]: %ld\n", i, j, matriz[i][j]);
+                    // printf("adding to parcial matrix. [%d, %d]: %f\n", g, h, matriz[i][j]);
                     parcial[g][h] = matriz[i][j];
                     h++;
                     if(h > nextSize-1){
@@ -72,18 +73,25 @@ double cofactor(double **matriz, int p, int n){
                 }
             }
         }
-        printMatrix(parcial, N);
-        return parcial[0][0];
+        // printMatrix(parcial, n-1);
+        double calc = 0;
+        for (int i=0; i<nextSize; i++){
+            // printf("next cofactors: %f\n", parcial[0][i]);
+            calc += matriz[0][p] * pow(-1, p) * cofactor(parcial, i, nextSize);
+        }
+        return calc;
     }
 }
 
 double determinant(double **matriz){
     double det = 0;
     double **m2;
-    cofactor(matriz, 1, N);
-    // for(int i = 0; i < N; i++){ // p/ cada elemento da primeira linha da matriz
-    //     cofactor(matriz, i, N);
-    // }
+    // det = cofactor(matriz, 0, N);
+    for(int i = 0; i < N; i++){ // p/ cada elemento da primeira linha da matriz
+        double cof = cofactor(matriz, i, N);
+        printf("Cof of %f: %f\n", matriz[0][i], cof);
+        det += cof;
+    }
     return det;
 }
 
@@ -92,21 +100,14 @@ int main(){
     
     double **m1 = createTestMatrix(N);
     
-
-    // m1[0][0] = (double) 1;
-    // m1[0][1] = (double) 2;
-    // m1[0][2] = (double) 3;
-    // m1[1][0] = (double) 4;
-    // m1[1][1] = (double) 5;
-    // m1[1][2] = (double) 6;
-    // m1[2][0] = (double) 7;
-    // m1[2][1] = (double) 8;
-    // m1[2][2] = (double) 9;
     printf("Calculating Determinant for the matrix: \n");
     printMatrix(m1, N);
 
-    determinant(m1);
-    // std::cout << determinant(m1, n) << std::endl;
+    m1[0][0] = 3;
+    m1[0][1] = 33;
+    m1[1][0] = 333;
+    m1[1][1] = 3333;
+    std::cout << determinant(m1) << std::endl;
 
     deleteMatrix(m1, N);
 }
