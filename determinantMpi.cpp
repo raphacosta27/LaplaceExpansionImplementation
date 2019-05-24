3ec3f0cacbe9b3face233d7056830d86daa5b32c
Raphael Costa <rapha2706@hotmai..com>
2019-05-24 12:02:37 -0300

    recommit da versao final do mpi

--- determinantMpi.cpp
+++ determinantMpi.cpp
@@ -1,12 +1,9 @@
 #include <iostream>
 #include <cmath>
 #include <mpi.h>
+#include <vector>
 
 using namespace std;
-
-// #define N 2
-#define ERROR 0
-#define READY 1
 
 double ** createTestMatrix(int n){
     double **m = new double *[n];
@@ -78,51 +75,90 @@ double cofactor(double **matriz, int p, int n){
     }
 }
 
-
+typedef struct Worker{
+    int rank;
+    int jobs;
+    std::vector<int> positions;
+} Worker;
 
 int main(int argc, char** argv){
     if (argc != 2) {
-        std::cerr << "Uso: ./determinantMpi tamanho matriz\n";
+        std::cerr << "Uso: ./gettingProcesses tamanho matriz\n";
         exit(1);
     }
     int N = atoi(argv[1]);
+
     MPI_Init(&argc, &argv);
     int world_rank; MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
-    int world_size; MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+    int n_processors; MPI_Comm_size(MPI_COMM_WORLD, &n_processors);
 
     double **m1;
-    int ready = 0;
-    double cof;
-    
+
+    Worker workers[n_processors-1];
+    int remaining_processes = N;
+    int remaining_workers = n_processors-1;
+    std::vector<int> cofactors (N);
+    for (int i=0; i<N; ++i) cofactors[i]=i;
+    for (int i = 0; i < n_processors-1; i++){
+        int jobs_to_do = ceil(remaining_processes/remaining_workers);
+        workers[i].rank = i+1;
+        workers[i].jobs = jobs_to_do;
+        remaining_processes -= jobs_to_do;
+        remaining_workers--;
+        for(int j = 0; j < jobs_to_do; j++){
+            workers[i].positions.insert(workers[i].positions.begin(), cofactors[0]);
+            cofactors.erase(cofactors.begin());
+        }
+    }
+    double det_local = 0;
+    double det_total = 0;
+
     if(world_rank == 0){
         m1 = createTestMatrix(N);
-        printf("MASTER: Calculating Determinant for the matrix: \n");
-        printMatrix(m1, N);
 
-        printf("MASTER: Broadcasting...\n");
-        
         for(int i = 0; i<N; i++) {
             MPI_Bcast(&(m1[i][0]), N, MPI_DOUBLE, 0, MPI_COMM_WORLD);
         }
-
-        double det = 0;
-        for(int i = 0; i < N; i++){
-            MPI_Recv(&cof, 1, MPI_DOUBLE, i+1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-            det += m1[0][i] * cof;
-        }
         
-        std::cout << det << std::endl;
-        deleteMatrix(m1, N);
     }
     else{
-        printf("%d: waiting for the beggining flag\n", world_rank);
         m1 = alocMatrix(N);
         for (int i = 0; i < N; i++){
             MPI_Bcast(&(m1[i][0]), N, MPI_DOUBLE, 0, MPI_COMM_WORLD);
         }
-        printf("%d: starting computing, p: %d\n", world_rank, world_rank-1);
-        cof = cofactor(m1, world_rank-1, N);
-        MPI_Send(&cof, 1, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD);
+
+        int workers_index = world_rank-1;
+        int jobs = workers[workers_index].jobs;
+        
+        for (int j = 0; j < jobs; j++){
+            int pos = workers[workers_index].positions[j];
+            det_local += m1[0][pos] * cofactor(m1, pos, N);
+        }
     }
+
+    MPI_Reduce(&det_local, &det_total, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+
+    if(world_rank == 0){
+        printf("----------WORKERS RELAT ----------\n");
+        for (int i = 0; i < n_processors-1; i++){
+            printf("Worker %d\n", workers[i].rank);
+            printf("Jobs: %d\n", workers[i].jobs);
+            if(workers[i].jobs > 0){
+                printf("Positions in the first line calculated: ");
+                for (int j = 0; j < workers[i].jobs; j++){
+                    printf("%d ", workers[i].positions[j]);
+                }
+                printf("\n");
+            }
+            printf("-------\n");
+        }
+        printf("----------FINAL RELAT----------\n");
+        printf("Number of workers: %d\n", n_processors-1);
+        printf("Calculated Determinant for the matrix: \n");
+        printMatrix(m1, N);
+        printf("DETERMINANT RESULT: %f\n", det_total);
+    }
+
+    deleteMatrix(m1, N);
     MPI_Finalize();
 }
